8e6d53002166caf0ddd6189b8fb3b62f2bc0435c
Carlos Gil <carlogilterriente@gmail.com>
2017-09-28 18:12:04 +0200

    Creo un marcador pero sin imagen,es decir,en el codigo está,pero en el mapa no sale porque no le he añadido imagen

--- codigo.js
+++ codigo.js
@@ -7,3 +7,14 @@ L.tileLayer(
         attribution: '&copy; ' + mapLink + ' Contributors',
         maxZoom: 18,
         }).addTo(map);
+var LeafIcon = L.Icon.extend({
+		options: {
+			iconSize:     [20, 30],/*Estas son las propedades del icono*/
+			iconAnchor:   [22, 94],
+			popupAnchor:  [-3, -76]
+		}
+	});
+
+	var marcador = new LeafIcon({iconUrl: ''}); /*creamo la variable marcador que es un nuevo objeto de LeafIcon */
+
+	L.marker([37.144229, -3.622781], {icon: marcador}).addTo(map); /* Añade el marcador con nombre "icono al mapa,coon las coordenadas que queremos*/
